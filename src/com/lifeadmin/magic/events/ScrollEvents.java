50b2a07ac5d28cec495b5a0388f5a5a3809f52f2
LarsKta <lars.kra2001@gmail.com>
2022-06-22 01:08:04 +0200

    closes #1 - added tp cooldown

--- src/com/lifeadmin/magic/events/ScrollEvents.java
+++ src/com/lifeadmin/magic/events/ScrollEvents.java
@@ -4,11 +4,14 @@ import com.lifeadmin.magic.Magic;
 import com.lifeadmin.magic.items.ItemManager;
 import com.lifeadmin.magic.staticFunctions.Chat;
 import org.bukkit.*;
+import org.bukkit.entity.Firework;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataContainer;
@@ -17,15 +20,42 @@ import org.bukkit.persistence.PersistentDataType;
 import java.util.Arrays;
 
 public class ScrollEvents implements Listener {
+    private static boolean tpAgain = true;
+
+    /**
+     * Function to prevent a player from
+     * tp again for a certain time
+     * @param cooldown in miliseconds
+     */
+    public static void setTeleportCooldown(int cooldown) {
+        new java.util.Timer().schedule(
+                new java.util.TimerTask() {
+                    @Override
+                    public void run() {
+                        // your code here
+                        tpAgain = true;
+                    }
+                },
+                cooldown
+        );
+    }
 
     @EventHandler
-    public static void onRightClick(PlayerInteractEvent event) {
-        if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-            if (event.getItem() != null) {
+    public static void onRightClick(PlayerToggleSneakEvent event) {
+        if (!tpAgain) {
+            Chat.chatError(event.getPlayer(), "You cannot use another scroll for some time!");
+            return;
+        }
+
+        ItemStack item = event.getPlayer().getInventory().getItemInMainHand();
+
+//        while (event.getPlayer().getWalkSpeed() == 0) {
+
+            if (item.getType() != Material.AIR) {
                 Player player = event.getPlayer();
-                PersistentDataContainer data = event.getItem().getItemMeta().getPersistentDataContainer();
+                PersistentDataContainer data = item.getItemMeta().getPersistentDataContainer();
                 if (!(data.has(new NamespacedKey(Magic.getPlugin(), "cords"), PersistentDataType.INTEGER_ARRAY))) {
-                    if (event.getItem().getItemMeta().equals(ItemManager.scrollOfTeleportation.getItemMeta())) {
+                    if (item.getItemMeta().equals(ItemManager.scrollOfTeleportation.getItemMeta())) {
                         Chat.chatError(player, "There was no destination set for this scroll!");
                         return;
                     }
@@ -48,6 +78,9 @@ public class ScrollEvents implements Listener {
                         Location loc = new Location(destinationWorld, x + 0.5, y, z + 0.5); // 0.5 places you in the middle of the Block
                         player.teleport(loc);
                         Chat.chatSuccess(player, "Teleport successful");
+                        player.getInventory().removeItem(item);
+                        tpAgain = false;
+                        ScrollEvents.setTeleportCooldown(5000);
                     } else {
                         Chat.chatError(player, "You are too far away from your Destination!");
                         Chat.chatWarning(player, "Your Destination: " +  Arrays.toString(cords));
@@ -57,7 +90,7 @@ public class ScrollEvents implements Listener {
                     Chat.chatError(player, "You cannot teleport through worlds!");
                     Chat.chatWarning(player, "Destination world: " + ChatColor.AQUA +  destinationWorld.getName());
                 }
-            }
+//            }
         }
     }
 
